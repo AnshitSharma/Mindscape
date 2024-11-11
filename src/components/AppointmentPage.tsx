01de39372d96af0d5f57e53da7014b87138aa39a
AnshitSharma <150780358+AnshitSharma@users.noreply.github.com>
2024-11-11 12:37:21 +0530

    image bug fixed

--- src/components/AppointmentPage.tsx
+++ src/components/AppointmentPage.tsx
@@ -312,7 +312,7 @@ const AppointmentPage: React.FC = () => {
             
             <div className="flex items-center justify-center">
               <img
-                src="/src/assets/doctorApp.png"
+                src="src/assets/doctorApp.png"
                 alt="Appointment illustration"
                 className="max-w-full h-auto"
               />
